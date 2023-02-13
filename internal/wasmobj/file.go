b77ef37d943aeceec92bcfa89f4c8c4b2579c0c2
Egon Elbre <egonelbre@gmail.com>
2023-02-13 21:23:46 +0200

    internal/wasmobj: switch to wabin

--- internal/wasmobj/file.go
+++ internal/wasmobj/file.go
@@ -1,16 +1,14 @@
 package wasmobj
 
 import (
-	"bytes"
 	"debug/dwarf"
 	"fmt"
 	"os"
 	"sort"
 	"strings"
 
-	wasmdisasm "github.com/go-interpreter/wagon/disasm"
-	"github.com/go-interpreter/wagon/wasm"
-	"github.com/go-interpreter/wagon/wasm/operators"
+	"github.com/tetratelabs/wabin/binary"
+	"github.com/tetratelabs/wabin/wasm"
 
 	"loov.dev/lensm/internal/disasm"
 )
@@ -31,11 +29,13 @@ func (file *File) Funcs() []disasm.Func { return file.funcs }
 // Func contains information about the executable.
 type Func struct {
 	obj      *File
-	fn       *wasm.Function
+	index    wasm.Index
+	name     string
+	code     *wasm.Code
 	sortName string
 }
 
-func (fn *Func) Name() string { return fn.fn.Name }
+func (fn *Func) Name() string { return fn.name }
 
 func (file *File) Close() error {
 	return nil
@@ -49,71 +49,25 @@ func Load(path string) (*File, error) {
 		return nil, err
 	}
 
-	module, err := wasm.ReadModule(bytes.NewReader(data),
-		func(name string) (*wasm.Module, error) {
-			return nil, fmt.Errorf("not found %q", name)
-		})
+	module, err := binary.DecodeModule(data, wasm.CoreFeaturesV2)
 	if err != nil {
 		return nil, err
 	}
 	obj.module = module
 
-	dwarfdata, err := dwarf.New(
-		module.Custom(".debug_abbrev").Data,
-		nil,
-		nil,
-		module.Custom(".debug_info").Data,
-		module.Custom(".debug_line").Data,
-		nil,
-		module.Custom(".debug_ranges").Data,
-		module.Custom(".debug_str").Data,
-	)
-	if err != nil {
-		return nil, err
-	}
+	tryParseDWARF(module)
 
-	rd := dwarfdata.Reader()
-	for {
-		entry, err := rd.Next()
-		if entry == nil && err == nil {
-			continue
-		}
-		if err != nil {
-			fmt.Println(err)
-			break
-		}
-		if entry.Tag == dwarf.TagCompileUnit {
-			fmt.Println("loading", entry.Field)
-			lrd, err := dwarfdata.LineReader(entry)
-			if err != nil {
-				fmt.Println(err)
-				break
-			}
+	fmt.Println("LOCAL NAMES", module.NameSection.LocalNames)
+	fmt.Println("FUNC NAMES", module.NameSection.FunctionNames)
 
-			for _, fln := range lrd.Files() {
-				fmt.Println(fln)
-			}
-
-			var lineEntry dwarf.LineEntry
-			for lrd.Next(&lineEntry) == nil {
-				fmt.Println(
-					lineEntry.Address,
-					lineEntry.Line,
-					lineEntry.Column,
-					lineEntry.File.Name,
-				)
-			}
-		}
-	}
-
-	obj.dwarf = dwarfdata
-
-	for _, fn := range module.FunctionIndexSpace {
-		fn := fn
+	for i, fnname := range module.NameSection.FunctionNames {
+		code := module.CodeSection[i]
 		sym := &Func{
 			obj:      obj,
-			fn:       &fn,
-			sortName: strings.ToLower(fn.Name),
+			index:    fnname.Index,
+			name:     fnname.Name,
+			code:     code,
+			sortName: strings.ToLower(fnname.Name),
 		}
 		obj.funcs = append(obj.funcs, sym)
 	}
@@ -130,6 +84,23 @@ func (fn *Func) Load(opts disasm.Options) *disasm.Code {
 }
 
 func (file *File) LoadCode(fn *Func, opts disasm.Options) *disasm.Code {
+	code := &disasm.Code{
+		Name: fn.name,
+	}
+
+	// TODO: https://www.w3.org/TR/2019/REC-wasm-core-1-20191205/#binary-instr
+
+	for i, b := range fn.code.Body {
+		code.Insts = append(code.Insts, disasm.Inst{
+			PC:   uint64(i),
+			Text: fmt.Sprintf("BYTE 0x%0x2", b),
+		})
+	}
+	return code
+}
+
+/*
+func (file *File) LoadCode(fn *Func, opts disasm.Options) *disasm.Code {
 	dis, err := wasmdisasm.NewDisassembly(*fn.fn, file.module)
 	if err != nil {
 		return &disasm.Code{Name: err.Error()}
@@ -145,7 +116,6 @@ func (file *File) LoadCode(fn *Func, opts disasm.Options) *disasm.Code {
 
 	return code
 }
-
 func (file *File) toInstr(dis *wasmdisasm.Disassembly, i int, ix wasmdisasm.Instr) disasm.Inst {
 	inst := disasm.Inst{
 		PC:   uint64(i),
@@ -173,4 +143,64 @@ func (file *File) immediatesToString(xs []interface{}) string {
 		fmt.Fprintf(&str, " %v", im)
 	}
 	return str.String()
+}
+*/
+
+func tryParseDWARF(module *wasm.Module) {
+	customSectionData := func(name string) []byte {
+		for _, sec := range module.CustomSections {
+			if sec.Name == name {
+				return sec.Data
+			}
+		}
+		return nil
+	}
+
+	dwarfdata, err := dwarf.New(
+		customSectionData(".debug_abbrev"),
+		customSectionData(".debug_aranges"),
+		customSectionData(".debug_frame"),
+		customSectionData(".debug_info"),
+		customSectionData(".debug_line"),
+		customSectionData(".debug_pubnames"),
+		customSectionData(".debug_ranges"),
+		customSectionData(".debug_str"),
+	)
+	if err != nil {
+		fmt.Println("ERROR", err)
+		return
+	}
+
+	rd := dwarfdata.Reader()
+	for {
+		entry, err := rd.Next()
+		if entry == nil && err == nil {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		if entry.Tag == dwarf.TagCompileUnit {
+			lrd, err := dwarfdata.LineReader(entry)
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+
+			for _, fln := range lrd.Files() {
+				fmt.Println(fln)
+			}
+
+			var lineEntry dwarf.LineEntry
+			for lrd.Next(&lineEntry) == nil {
+				fmt.Println(
+					lineEntry.Address,
+					lineEntry.Line,
+					lineEntry.Column,
+					lineEntry.File.Name,
+				)
+			}
+		}
+	}
 }
