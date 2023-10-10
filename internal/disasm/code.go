168683efb446a949d7437560199b1b1811142261
Egon Elbre <egonelbre@gmail.com>
2023-10-10 17:42:23 +0300

    internal/disasm: document the main interface

--- internal/disasm/code.go
+++ internal/disasm/code.go
@@ -1,36 +1,63 @@
 package disasm
 
+// Code combines the disassembly and the source code mapping.
 type Code struct {
+	// Name is the name of the code block, e.g. function or method name.
 	Name string
+	// File is where the code is located.
 	File string
 
-	Insts   []Inst
+	// Insts is the slice of a all instructions in the code.
+	Insts []Inst
+	// MaxJump is the maximum layers of jumps for the codeblock.
+	// This is used to determine how much padding is created for
+	// the jump lines.
 	MaxJump int
 
+	// Source is the slice of a codeblocks that were used to create the instructions.
 	Source []Source
 }
 
 // Inst represents a single instruction.
 type Inst struct {
-	PC   uint64
+	// PC is the program counter, usually offset in the binary.
+	PC uint64
+	// Text is the textual representation of this instruction.
 	Text string
+	// File is the location where this instruction was compiled from.
 	File string
+	// Line is the line in the file where this instruction was compiled from.
 	Line int
 
-	RefPC     uint64
+	// RefPC is a reference to another program counter, e.g. a call.
+	RefPC uint64
+	// RefOffset is a reference to a relative jump.
 	RefOffset int
-	RefStack  int
+	// RefStack is the depth that the jump line should be drawn at.
+	RefStack int
 
+	// Call is a named target that should be present in Funcs.
+	// This is used to make the instruction clickable and follow to the
+	// called target.
 	Call string
 }
 
+// Source represents code from a single file.
 type Source struct {
-	File   string
+	// File is the file name for the source code.
+	File string
+	// Blocks is a slice of blocks that were used for compiling the instructions.
 	Blocks []SourceBlock
 }
 
+// SourceBlock represents a single sequential codeblock that references the instructions.
 type SourceBlock struct {
+	// LineRange is the range of lines that it references from the file.
 	LineRange
-	Lines   []string
-	Related [][]LineRange // for each line, a range index in Code.Insts
+	// Lines are textual representation of the source starting from `LineRange.From`.
+	Lines []string
+	// Related contains a set of ranges in the instructions.
+	// e.g. for source code Lines[5] there will be drawn relation shapes to each
+	// instructions `for _, r := range Related[5] { draw(Insts[r.From:r.To]) }`
+	Related [][]LineRange
 }
--- internal/disasm/file.go
+++ internal/disasm/file.go
@@ -2,15 +2,23 @@ package disasm
 
 // File represents an object file, a module or anything that contains functions.
 type File interface {
+	// Close closes the underlying data.
 	Close() error
+	// Funcs enumerates all the visualizable code blocks.
 	Funcs() []Func
 }
 
+// Func represents a function or method that can be independently rendered.
 type Func interface {
+	// Name is the name of the func.
 	Name() string
+	// Load loads the source code and disassembles it.
 	Load(opt Options) *Code
 }
 
+// Options defines configuration for loading the func.
 type Options struct {
+	// Context is the number of lines that should be additionally included for context.
+	// This can often contain function documentation.
 	Context int
 }
