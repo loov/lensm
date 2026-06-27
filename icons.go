33b3031ffe226573056a6dcde8a8e06e6f3a3e4b
Ilya Torbin <il.torbin@gmail.com>
2026-06-27 18:37:28 +0300

    remove redundant code view controls

--- icons.go
+++ icons.go
@@ -5,18 +5,6 @@ import (
 	"golang.org/x/exp/shiny/materialdesign/icons"
 )
 
-// OpenInNewIcon is used for opening Code in a new window.
-var OpenInNewIcon = func() *widget.Icon {
-	icon, _ := widget.NewIcon(icons.ActionOpenInNew)
-	return icon
-}()
-
-// CopyIcon is used for copying assembly to the clipboard.
-var CopyIcon = func() *widget.Icon {
-	icon, _ := widget.NewIcon(icons.ContentContentCopy)
-	return icon
-}()
-
 // CommentIcon is used for the current assembly line comment editor.
 var CommentIcon = func() *widget.Icon {
 	icon, _ := widget.NewIcon(icons.EditorModeComment)
@@ -26,16 +14,5 @@ var CommentIcon = func() *widget.Icon {
 // SettingsIcon is used for application settings.
 var SettingsIcon = func() *widget.Icon {
 	icon, _ := widget.NewIcon(icons.ActionSettings)
-	return icon
-}()
-
-// BackIcon and ForwardIcon navigate through visited functions.
-var BackIcon = func() *widget.Icon {
-	icon, _ := widget.NewIcon(icons.NavigationArrowBack)
-	return icon
-}()
-
-var ForwardIcon = func() *widget.Icon {
-	icon, _ := widget.NewIcon(icons.NavigationArrowForward)
 	return icon
 }()
